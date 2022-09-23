8eb6f5e231b9aa8fd870b138a742e982a9163a95
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-09-23 12:48:25 +0200

    update

--- js/script.js
+++ js/script.js
@@ -38,3 +38,21 @@ var swiper = new Swiper(".image-slider", {
       },
     },
   });
+
+  let mybutton = document.getElementById("buttonTop");
+
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
